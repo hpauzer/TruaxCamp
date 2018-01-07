069f88dab9339ddd560d13144d1d18952ee1f754
sunnypzr <heather.pauzer@gmail.com>
2018-01-07 14:44:14 -0600

    working on breaking up components from app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,163 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './style/App.css';
+import Navbar from './components/Navbar.js';
+import Panel from './components/Panel.js';
+import Footer from './components/Footer.js';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+ return  ();
+
+
+       <div id="index-banner" class="parallax-container">
+          <div class="section no-pad-bot">
+            <div class="container">
+              <br>
+                <br>
+                  <h1 class="header center teal-text text-lighten-2"></h1>
+                  <div class="row center">
+                    <h5 class="header col s12 light"></h5>
+                  </div>
+                  <div class="row right"></div>
+                  <br>
+                    <br></div>
+                  </div>
+                  <div class="parallax fit">
+                    <img
+                      class="responsive-img"
+                      src="background1.jpg"
+                      alt="Unsplashed background img 1"></div>
+                  </div>
+
+                  <div class="container">
+                    <div class="section">
+
+                      <!--   Icon Section   -->
+                      <div class="row">
+                        <div class="col s12 m4">
+                          <div class="icon-block">
+                            <h2 class="center amber-text">
+                              <i class="material-icons">album</i>
+                            </h2>
+                            <h5 class="center amber-text">Our</br>Mission</h5>
+
+                          <p class="grey-text light">Creating a lifelong desire to learn, listen and perform music.
+                          </p>
+                        </div>
+                      </div>
+
+                      <div class="col s12 m4">
+                        <div class="icon-block">
+                          <h2 class="center amber-text">
+                            <i class="material-icons">audiotrack</i>
+                          </h2>
+                          <h5 class="center amber-text">Truax Troubadours</h5>
+
+                          <p class="grey-text light">Join this select group of performers. Auditions coming soon!</p>
+                        </div>
+                      </div>
+
+                      <div class="col s12 m4">
+                        <div class="icon-block">
+                          <h2 class="center amber-text">
+                            <i class="material-icons">insert_invitation</i>
+                          </h2>
+                          <h5 class="center amber-text">Save</br>the Date</h5>
+
+                        <p class="grey-text light">2018 summer camp will be held from June 18th - 23rd in Dallas, Texas.</p>
+                      </div>
+                    </div>
+                  </div>
+
+                </div>
+              </div>
+
+              <div class="parallax-container valign-wrapper">
+                <div class="section no-pad-bot">
+                  <div class="container">
+                    <div class="row center">
+                      <h5 class="header col s12 amber-text light">“Music gives a soul to the universe,
+                        wings to the mind, flight to the imagination and life to everything.” ― Plato</h5>
+                    </div>
+                  </div>
+                </div>
+                <div class="parallax">
+                  <img src="background2.jpg" alt="Unsplashed background img 2"></div>
+                </div>
+
+                <div class="container">
+                  <div class="section">
+
+                    <div class="row">
+                      <div class="col s12 center">
+                        <h3>
+                          <i class="mdi-content-send grey-text"></i>
+                        </h3>
+                        <a class="amber waves-effect waves-light btn">Donate</a>
+  
+                        <p class="grey-text left-align light">Truax School of Trumpet is a 501(c)3
+                          organization. Your contributions fund scholarships that provide opportunities
+                          for students to learn, listen and perform at camp and throughout the year.</p>
+                      </div>
+                    </div>
+
+                  </div>
+                </div>
+
+                <div class="parallax-container valign-wrapper">
+                  <div class="section no-pad-bot">
+                    <div class="container">
+                      <div class="row center">
+                        <h5 class="header col s12 amber-text light">“Music can change the world because it can change people.”
+                        </br>― Bono</h5>
+                    </div>
+                  </div>
+                </div>
+                <div class="parallax">
+                  <img src="background3.jpg" alt="Unsplashed background img 3"></div>
+                </div>
+
+                <footer class="page-footer grey">
+                  <div class="container">
+                    <div class="row">
+                      <div class="col l6 s12">
+                        <h5 class="amber-text">Founder's Bio</h5>
+                        <p class="white-text text-lighten-4">We are a team of college students working
+                          on this project like it's our full time job. Any amount would help support and
+                          continue development on this project and is greatly appreciated.</p>
+
+                      </div class="grey">
+                      <div class="col l3 s12 grey">
+                        <h5 class="amber-text">Settings</h5>
+                        <ul>
+                          <li>
+                            <a class="amber-text" href="#!">Link 1</a>
+                          </li>
+                          <li>
+                            <a class="amber-text" href="#!">Link 2</a>
+                          </li>
+                          <li>
+                            <a class="amber-text" href="#!">Link 3</a>
+                          </li>
+                          <li>
+                            <a class="amber-text" href="#!">Link 4</a>
+                          </li>
+                        </ul>
+                      </div>
+                      <div class="col l3 s12 grey">
+                        <h5 class="amber-text">Connect</h5>
+
+                      </div>
+                    </div>
+                  </div>
+                  <div class="footer-copyright">
+                    <div class="container">
+                      Made by
+                      <a class="amber-text text-lighten-3" href="http://materializecss.com">Materialize</a>
+                    </div>
+                  </div>
+                </footer>`
+    
   }
 }
 
